c862a98057665880016a919296448aec01b65d67
Blazej SLEBODA <contact@blazejsleboda.fr>
2021-10-19 22:39:50 +0200

    Add xcode 13 ios app swiftui

---
+++ Xcode_13_0/ios_app_swiftui/ios_app_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_app_swiftui
+//
+//  Created by rootnode on 19/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode_13_0/ios_app_swiftui/ios_app_swiftui/ios_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_swiftuiApp.swift
+//  ios_app_swiftui
+//
+//  Created by rootnode on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
